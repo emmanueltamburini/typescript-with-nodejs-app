dae955e8627f9114c40137b2bad35eae2c1f3b49
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-01-17 21:32:22 -0400

    user model has created

--- constants/paramsQueries.constant.ts
+++ constants/paramsQueries.constant.ts
@@ -1,2 +1,3 @@
 export const ID = 'id';
+export const USER = 'user';
 export const DIALECT_BD = 'mysql';
---
+++ db/user.ts
@@ -0,0 +1,22 @@
+import { DataTypes } from 'sequelize';
+import db from './connection';
+import { capitalize } from '../helpers/utils';
+import { USER } from '../constants/paramsQueries.constant';
+
+const User = db?.define(capitalize(USER), {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+});
+
+export default User;
